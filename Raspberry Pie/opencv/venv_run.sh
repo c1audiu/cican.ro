094d47a2d90dbe9217b9fd7a04800566b26ff4ee
Claudiu <claudiu@cican.ro>
2018-05-09 21:12:13 +0300

    Create venv_run.sh

---
+++ Raspberry Pie/opencv/venv_run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# if virtualenvwrapper.sh is in your PATH (i.e. installed with pip)
+source `which virtualenvwrapper.sh`
+#source /path/to/virtualenvwrapper.sh # if it's not in your PATH
+workon $1
+python $2
+deactivate
+#venv_run.sh my_virtualenv /path/to/script.py
