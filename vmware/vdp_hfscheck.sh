5cbe4e6bd24955cf765233f42d6904cde42cf8d3
Claudiu <claudiu@cican.ro>
2018-05-11 20:05:23 +0300

    Create vdp_hfscheck.sh

---
+++ vmware/vdp_hfscheck.sh
@@ -0,0 +1,33 @@
+#FIX "VDP: [001] The most recent checkpoint for the VDP appliance is outdated"
+
+#ssh root@vdp
+
+dpnctl stop
+
+mount | grep data
+/dev/sde1 on /data01 type xfs (rw,noatime)
+/dev/sdf1 on /data02 type xfs (rw,noatime)
+/dev/sdg1 on /data03 type xfs (rw,noatime)
+
+umount /dev/sde1 && umount /dev/sdf1 && umount /dev/sdg1
+
+xfs_check /dev/sde1 && xfs_check /dev/sdf1 && xfs_check /dev/sdg1
+
+mount -a
+
+mount | grep data
+/dev/sde1 on /data01 type xfs (rw,noatime)
+/dev/sdf1 on /data02 type xfs (rw,noatime)
+/dev/sdg1 on /data03 type xfs (rw,noatime)
+
+dpnctl start all
+
+dpnctl stop maint
+
+avmaint checkpoint --ava
+
+avmaint hfscheck --full --ava
+
+avmaint hfscheckstatus --ava | grep 'status\|result'
+
+dpnctl start maint
